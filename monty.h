c84c77835dda5e00be3f43c40263b7469a358399
Collins331 <ochiengcollinsoduor2@gmail.com>
2023-09-22 15:01:36 +0300

    modified header file

--- monty.h
+++ monty.h
@@ -75,5 +75,8 @@ void divd(stack_t **stack, unsigned int line_number);
 void mul(stack_t **stack, unsigned int line_number);
 void mod(stack_t **stack, unsigned int line_number);
 void pchar(stack_t **stack, unsigned int line_number);
+void pstr(stack_t **stack, unsigned int line_number);
+void rotl(stack_t **stack, unsigned int line_number);
+void rotr(stack_t **stack, unsigned int line_number);
 
 #endif
