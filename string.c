41f659289f357616de3a239122545e797d8ada62
Collins Oduor Ochieng <125954874+Collins331@users.noreply.github.com>
2023-09-22 17:59:45 +0300

    Update string.c

--- string.c
+++ string.c
@@ -10,7 +10,7 @@ void pstr(stack_t **stack, unsigned int line_number)
 	stack_t *temp = (*stack);
 	(void) line_number;
 
-	while (temp && temp->n != 0 && (temp->n > 0 && temp->n <= 127))
+	while (temp && temp->n != 0 && (temp->n >= 0 && temp->n <= 127))
 	{
 		printf("%c", temp->n);
 		temp = temp->next;
